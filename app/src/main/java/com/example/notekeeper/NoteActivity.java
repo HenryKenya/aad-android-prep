07b61d8181e263d9576615f570dfd485f209b9d0
HenryKenya <henry.onyango@outlook.com>
2020-02-14 10:49:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.notekeeper;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}

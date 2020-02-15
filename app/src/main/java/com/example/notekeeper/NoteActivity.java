1e2f4dfb20f91627abf0a79826ca4ad89fc3c77a
HenryKenya <henry.onyango@outlook.com>
2020-02-15 22:52:10 +0300

    implement edit-in-place model for saving notes

--- app/src/main/java/com/example/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -41,8 +41,33 @@ public class NoteActivity extends AppCompatActivity {
         textNoteTitle = findViewById(R.id.edit_note_title);
         textNoteText = findViewById(R.id.edit_note_text);
 
-        if (!isNewNote) {
+        if (isNewNote) {
+            createNewNote();
+        } else {
             displayNote(spinnerCourses, textNoteTitle, textNoteText);
+        }
+    }
+
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        int notePostion = dm.createNewNote();
+        note = DataManager.getInstance().getNotes().get(notePostion);
+    }
+
+    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courseIndex = courses.indexOf(note.getCourse());
+        spinnerCourses.setSelection(courseIndex);
+        textNoteTitle.setText(note.getTitle());
+        textNoteText.setText(note.getText());
+    }
+
+    private void readDisplayStateValues() {
+        Intent intent = getIntent();
+        int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
+        isNewNote = position == POSITION_NOT_SET;
+        if (!isNewNote) {
+            note = DataManager.getInstance().getNotes().get(position);
         }
     }
 
@@ -50,7 +75,7 @@ public class NoteActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.note_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        return true;
     }
 
     @Override
@@ -77,20 +102,15 @@ public class NoteActivity extends AppCompatActivity {
 
     }
 
-    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
-        List<CourseInfo> courses = DataManager.getInstance().getCourses();
-        int courseIndex = courses.indexOf(note.getCourse());
-        spinnerCourses.setSelection(courseIndex);
-        textNoteTitle.setText(note.getTitle());
-        textNoteText.setText(note.getText());
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveNote();
     }
 
-    private void readDisplayStateValues() {
-        Intent intent = getIntent();
-        int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
-        isNewNote = position == POSITION_NOT_SET;
-        if (!isNewNote) {
-            note = DataManager.getInstance().getNotes().get(position);
-        }
+    private void saveNote() {
+        note.setCourse((CourseInfo) spinnerCourses.getSelectedItem());
+        note.setTitle(textNoteTitle.getText().toString());
+        note.setText(textNoteText.getText().toString());
     }
 }

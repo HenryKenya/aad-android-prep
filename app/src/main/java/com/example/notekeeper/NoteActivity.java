383060ae97a3e1dd0ede849aee0ff1805f645ec6
HenryKenya <henry.onyango@outlook.com>
2020-02-15 18:16:27 +0300

    populate selected note in NoteActivity

--- app/src/main/java/com/example/notekeeper/NoteActivity.java
+++ app/src/main/java/com/example/notekeeper/NoteActivity.java
@@ -1,7 +1,9 @@
 package com.example.notekeeper;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import java.util.List;
 public class NoteActivity extends AppCompatActivity {
 
     public static final String NOTE_INFO = "com.example.notekeeper.NOTE_INFO";
+    private NoteInfo note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,25 @@ public class NoteActivity extends AppCompatActivity {
         ArrayAdapter<NoteInfo> adapterCourses = new ArrayAdapter(this, android.R.layout.simple_spinner_item, courses);
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCourses.setAdapter(adapterCourses);
+
+        readDisplayStateValues();
+
+        EditText textNoteTitle = findViewById(R.id.edit_note_title);
+        EditText textNoteText = findViewById(R.id.edit_note_text);
+
+        displayNote(spinnerCourses, textNoteTitle, textNoteText);
+    }
+
+    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courseIndex = courses.indexOf(note.getCourse());
+        spinnerCourses.setSelection(courseIndex);
+        textNoteTitle.setText(note.getTitle());
+        textNoteText.setText(note.getText());
+    }
+
+    private void readDisplayStateValues() {
+        Intent intent = getIntent();
+        note = intent.getParcelableExtra(NOTE_INFO);
     }
 }

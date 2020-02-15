1446fbda6ef0a8d685b3fce8be588c3a9a853ace
HenryKenya <henry.onyango@outlook.com>
2020-02-15 17:58:25 +0300

    make NoteInfo parcelable

--- app/src/main/java/com/example/notekeeper/NoteInfo.java
+++ app/src/main/java/com/example/notekeeper/NoteInfo.java
@@ -1,6 +1,9 @@
 package com.example.notekeeper;
 
-public final class NoteInfo {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public final class NoteInfo implements Parcelable {
     private CourseInfo mCourse;
     private String mTitle;
     private String mText;
@@ -9,6 +12,12 @@ public final class NoteInfo {
         mCourse = course;
         mTitle = title;
         mText = text;
+    }
+
+    private NoteInfo(Parcel parcel) {
+        mCourse = parcel.readParcelable(CourseInfo.class.getClassLoader()); // must provide class loader to readParcelable
+        mTitle = parcel.readString();
+        mText = parcel.readString();
     }
 
     public CourseInfo getCourse() {
@@ -59,4 +68,29 @@ public final class NoteInfo {
         return getCompareKey();
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int flags) {
+        parcel.writeParcelable(mCourse, 0);
+        parcel.writeString(mTitle);
+        parcel.writeString(mText);
+    }
+
+    public static final Parcelable.Creator<NoteInfo> CREATOR = new Parcelable.Creator<NoteInfo>() {
+
+        @Override
+        public NoteInfo createFromParcel(Parcel parcel) {
+            // must be in the same order as writeToParcel
+            return new NoteInfo(parcel);
+        }
+
+        @Override
+        public NoteInfo[] newArray(int size) {
+            return new NoteInfo[size];
+        }
+    };
 }

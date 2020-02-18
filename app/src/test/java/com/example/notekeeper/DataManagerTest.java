74d8cf4f70ca55e56820a17e2f44e03c788476d4
HenryKenya <henry.onyango@outlook.com>
2020-02-18 22:26:40 +0300

    refactor test to use pre-processing @Before and @BeforeClass

--- app/src/test/java/com/example/notekeeper/DataManagerTest.java
+++ app/src/test/java/com/example/notekeeper/DataManagerTest.java
@@ -1,17 +1,31 @@
 package com.example.notekeeper;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class DataManagerTest {
 
+    static DataManager dataManager;
+
+    @BeforeClass
+    public static void classSetUp() {
+        dataManager = DataManager.getInstance();
+    }
+
+    @Before
+    public void setUp() {
+        dataManager.getNotes().clear();
+        dataManager.initializeExampleNotes();
+    }
+
     @Test
     public void createNewNote() {
-        DataManager dataManager = DataManager.getInstance();
         final CourseInfo course = dataManager.getCourse("android_async");
         final String noteTitle = "Test note title";
-        final String noteText = "Test note text";
+        final String noteText = "This is the body text of my test note";
 
         int noteIndex = dataManager.createNewNote();
         NoteInfo newNote = dataManager.getNotes().get(noteIndex);
@@ -25,5 +39,31 @@ public class DataManagerTest {
         assertEquals(course, compareNote.getCourse());
         assertEquals(noteTitle, compareNote.getTitle());
         assertEquals(noteText, compareNote.getText());
+    }
+
+    @Test
+    public void findSimilarNotes() {
+        final CourseInfo course = dataManager.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText1 = "This is the body text of my test note";
+        final String noteText2 = "This is the body of my second test note";
+
+        int noteIndex1 = dataManager.createNewNote();
+        NoteInfo newNote1 = dataManager.getNotes().get(noteIndex1);
+        newNote1.setCourse(course);
+        newNote1.setTitle(noteTitle);
+        newNote1.setText(noteText1);
+
+        int noteIndex2 = dataManager.createNewNote();
+        NoteInfo newNote2 = dataManager.getNotes().get(noteIndex2);
+        newNote2.setCourse(course);
+        newNote2.setTitle(noteTitle);
+        newNote2.setText(noteText2);
+
+        int foundIndex1 = dataManager.findNote(newNote1);
+        assertEquals(noteIndex1, foundIndex1);
+
+        int foundIndex2 = dataManager.findNote(newNote2);
+        assertEquals(noteIndex2, foundIndex2);
     }
 }

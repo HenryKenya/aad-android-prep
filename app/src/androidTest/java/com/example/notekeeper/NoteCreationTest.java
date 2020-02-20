915f9861634cd89b3ac1bda4eec207030e9e81fd
HenryKenya <henry.onyango@outlook.com>
2020-02-20 22:15:31 +0300

    set up for testing note creation

--- app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
@@ -1,5 +1,20 @@
 package com.example.notekeeper;
 
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+    @Rule
+    public ActivityTestRule<NoteListActivity> noteListActivityActivityRule
+            = new ActivityTestRule(NoteListActivity.class);
+
+    @Test
+    public void createNewNote() {
+    }
 
 }

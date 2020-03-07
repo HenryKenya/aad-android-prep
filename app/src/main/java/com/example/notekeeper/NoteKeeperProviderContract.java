ba132287e2ccf0798c5b55156803c1eabb283d56
HenryKenya <henry.onyango@outlook.com>
2020-03-07 22:26:49 +0300

    create content provider contract class and implement methods

---
+++ app/src/main/java/com/example/notekeeper/NoteKeeperProviderContract.java
@@ -0,0 +1,37 @@
+package com.example.notekeeper;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public final class NoteKeeperProviderContract {
+    private NoteKeeperProviderContract() {
+    } // make class not creatable
+
+    public static final String AUTHORITY = "com.example.notekeeper";
+    public static final Uri AUTHORITY_URI = Uri.parse("content://" + AUTHORITY);
+
+    protected interface CourseIdColumns {
+        String COLUMN_COURSE_ID = "course_id";
+    }
+
+    protected interface CourseColumns {
+        String COLUMN_COURSE_TITLE = "course_title";
+    }
+
+    protected interface NotesColumns {
+        String COLUMN_NOTE_TITLE = "note_title";
+        String COLUMN_NOTE_TEXT = "note_text";
+    }
+
+    public static final class Courses implements BaseColumns, CourseColumns, CourseIdColumns {
+        public static final String PATH = "courses";
+        public static final Uri CONTENT_PATH = Uri.withAppendedPath(AUTHORITY_URI, PATH);
+
+    }
+
+    public static final class Notes implements BaseColumns, NotesColumns, CourseIdColumns {
+        public static final String PATH = "notes";
+        public static final Uri CONTENT_PATH = Uri.withAppendedPath(AUTHORITY_URI, PATH);
+
+    }
+}

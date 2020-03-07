66191acfc5544596d6c52e2aaa85de8d62384622
HenryKenya <henry.onyango@outlook.com>
2020-03-07 21:24:32 +0300

    query courses using content provider

---
+++ app/src/main/java/com/example/notekeeper/NoteKeeperProvider.java
@@ -0,0 +1,58 @@
+package com.example.notekeeper;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+import com.example.notekeeper.NoteKeeperDatabaseContract.CourseInfoEntry;
+
+public class NoteKeeperProvider extends ContentProvider {
+
+    NoteKeeperOpenHelper dbHelper;
+
+    public NoteKeeperProvider() {
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        // Implement this to handle requests to delete one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        // TODO: Implement this to handle requests for the MIME type of the data
+        // at the given URI.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        // TODO: Implement this to handle requests to insert a new row.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public boolean onCreate() {
+        dbHelper = new NoteKeeperOpenHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+        Cursor cursor;
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        cursor = db.query(CourseInfoEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+        return cursor;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection,
+                      String[] selectionArgs) {
+        // TODO: Implement this to handle requests to update one or more rows.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}

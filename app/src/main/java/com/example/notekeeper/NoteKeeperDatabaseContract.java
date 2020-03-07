0b382730b94c227505760bdfd87a839067e6195b
HenryKenya <henry.onyango@outlook.com>
2020-03-07 15:17:59 +0300

    add indices on SQL table open helper class

--- app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
@@ -11,6 +11,9 @@ public final class NoteKeeperDatabaseContract {
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
 
+        public static final String INDEX1 = TABLE_NAME + "_index1";
+        public static final String SQL_CREATE_INDEX1 = " CREATE INDEX "
+                + INDEX1 + " ON " + TABLE_NAME + "(" + COLUMN_COURSE_TITLE + ")";
 
         public static final String getQName(String columnName) {
             return TABLE_NAME + "." + columnName;
@@ -29,6 +32,10 @@ public final class NoteKeeperDatabaseContract {
         public static final String COLUMN_NOTE_TITLE = "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
         public static final String COLUMN_COURSE_ID = "course_id";
+
+        public static final String INDEX1 = TABLE_NAME + "_index1";
+        public static final String SQL_CREATE_INDEX1 = " CREATE INDEX "
+                + INDEX1 + " ON " + TABLE_NAME + "(" + COLUMN_NOTE_TITLE + ")";
 
         public static final String getQName(String columnName) {
             return TABLE_NAME + "." + columnName;

23e5302966fa5478c642618398b5568d4ea0969a
HenryKenya <henry.onyango@outlook.com>
2020-03-08 18:39:56 +0300

    add join in content provider contract class

--- app/src/main/java/com/example/notekeeper/NoteKeeperProviderContract.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperProviderContract.java
@@ -32,6 +32,8 @@ public final class NoteKeeperProviderContract {
     public static final class Notes implements BaseColumns, NotesColumns, CourseIdColumns {
         public static final String PATH = "notes";
         public static final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
+        public static final String EXPANDED_PATH = "notes_expanded";
+        public static final Uri CONTENT_EXPANDED_URI = Uri.withAppendedPath(AUTHORITY_URI, EXPANDED_PATH);
 
     }
 }

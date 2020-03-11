33586b6327748da8ae5387951bca82018e4a4921
HenryKenya <henry.onyango@outlook.com>
2020-03-11 11:21:07 +0300

    implement async task to do upload in job service

--- app/src/main/java/com/example/notekeeper/NoteUploaderJobService.java
+++ app/src/main/java/com/example/notekeeper/NoteUploaderJobService.java
@@ -1,16 +1,31 @@
 package com.example.notekeeper;
 
+import android.annotation.SuppressLint;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
+import android.net.Uri;
+import android.os.AsyncTask;
 
 public class NoteUploaderJobService extends JobService {
     public static final String EXTRA_DATA_URI = "com.example.notekeeper.extras.DATA_URI";
+    private NoteUploader noteUploader;
 
     public NoteUploaderJobService() {
     }
 
     @Override
     public boolean onStartJob(JobParameters params) {
+        @SuppressLint("StaticFieldLeak") AsyncTask<JobParameters, Void, Void> task = new AsyncTask<JobParameters, Void, Void>() {
+            @Override
+            protected Void doInBackground(JobParameters... backgroundParams) {
+                JobParameters jobParameters = backgroundParams[0];
+                String stringURI = jobParameters.getExtras().getString(EXTRA_DATA_URI);
+                Uri dataURI = Uri.parse(stringURI);
+                noteUploader.doUpload(dataURI);
+                return null;
+            }
+        };
+        noteUploader = new NoteUploader(this);
         return false;
     }
 

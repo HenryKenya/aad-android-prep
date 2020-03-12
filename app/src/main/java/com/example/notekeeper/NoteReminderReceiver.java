9df01b0531a869a2eba9a62d6199885250bd8309
HenryKenya <henry.onyango@outlook.com>
2020-03-12 13:17:25 +0300

    (refactor) implement notification in broadcast receiver

--- app/src/main/java/com/example/notekeeper/NoteReminderReceiver.java
+++ app/src/main/java/com/example/notekeeper/NoteReminderReceiver.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 
 public class NoteReminderReceiver extends BroadcastReceiver {
 
+    public static final String EXTRA_NOTE_TITLE = "com.example.notekeeper.extras.EXTRA_NOTE_TITLE";
+    public static final String EXTRA_NOTE_TEXT = "com.example.notekeeper.extras.EXTRA_NOTE_TEXT";
+    public static final String EXTRA_NOTE_ID = "com.example.notekeeper.extras.EXTRA_NOTE_ID";
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        String noteTitle = intent.getStringExtra(EXTRA_NOTE_TITLE);
+        String noteText = intent.getStringExtra(EXTRA_NOTE_TEXT);
+        int noteId = intent.getIntExtra(EXTRA_NOTE_ID, 0);
+
+        NoteReminderNotification.notify(context, noteTitle, noteText, noteId);
     }
 }

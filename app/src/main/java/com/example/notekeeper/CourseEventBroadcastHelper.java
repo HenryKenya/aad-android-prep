be15612d134b3360befe3f5cd56a3a262d161d4b
HenryKenya <henry.onyango@outlook.com>
2020-03-12 11:13:04 +0300

    (feature) implement broadcast helper class

---
+++ app/src/main/java/com/example/notekeeper/CourseEventBroadcastHelper.java
@@ -0,0 +1,19 @@
+package com.example.notekeeper;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class CourseEventBroadcastHelper {
+    public static final String ACTION_COURSE_EVENT = "com.example.notekeeper.action.ACTION_COURSE_EVENT";
+    public static final String EXTRA_COURSE_ID = "com.example.notekeeper.extra.COURSE_ID";
+    public static final String EXTRA_COURSE_MESSAGE = "com.example.notekeeper.extra.EXTRA_COURSE_MESSAGE";
+
+    public static void sendEventBroadcast(Context context, String courseId, String message) {
+
+        Intent intent = new Intent(ACTION_COURSE_EVENT);
+        intent.putExtra(EXTRA_COURSE_ID, courseId);
+        intent.putExtra(EXTRA_COURSE_MESSAGE, message);
+
+        context.sendBroadcast(intent);
+    }
+}
